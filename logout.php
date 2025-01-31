21e01229f207d08514126c8b64b6c85d620c7abb
loritedemolli <demollilorita@gmail.com>
2025-01-31 01:04:53 +0100

    Clear all session cookies

--- logout.php
+++ logout.php
@@ -4,7 +4,14 @@ session_start();
 session_unset();
 session_destroy();
 
-header ("Location: loginform.php");
+setcookie(session_name(), '', time() - 3600, '/');
 
+
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+header("Expires: Thu, 01 Jan 1970 00:00:00 GMT");
+
+header ("Location: login.php");
 exit;
 ?>
