2806a3c5a219cd886481787c2b3c5d06f0c971f7
loritedemolli <demollilorita@gmail.com>
2025-01-31 21:32:31 +0100

    Skincare permiresim

--- skincare.php
+++ skincare.php
@@ -6,41 +6,167 @@ if (!isset($_SESSION['user_id'])) {
 }
 include_once 'inc/header.php';
 ?>
-
-<div class="contact-container">
-    <h1 class="contact-title">Contact Us</h1>
-
-    <div class="contact-info">
-        <h2>Our Contact Information</h2>
-        <p>Feel free to reach out to us through the form below or the following:</p>
-        <ul>
-            <li>Email: <a href="mailto:support@divaglow.com">support@divaglow.com</a></li>
-            <li>Phone: <a href="tel:+1234567890">+1 234 567 890</a></li>
-            <li>Address: 120524 Main Street, Prishtinë, Kosova</li>
-        </ul>
+<section id="makeup-products">
+         <div class="intro">
+            <h2>Explore Our Premium Skincare Collection</h2>
+            <p>The best products of skincare. Make your skin glow with Diva Glow!</p>
+        </div>
+           <div class="brands-category">
+            <h3>Browse by Brand</h3>
+            <select id="brand-dropdown" class="dropdown-box">
+                <option value="" disabled selected>Select a Brand</option>
+                <option value="all">All Products</option>
+                <option value="fenty">Fenty Skin</option>
+                <option value="cerave">Cera Ve</option>
+                <option value="laroche">La Roche Posay</option>
+                <option value="ordinary">The Ordinary</option>
+            </select>
+     </div>
+     <div class="product-container">
+        <div class="produkti" data-brand="laroche">
+            <img src="assets/img/pages/skincare/larochecleanser.jpg" alt="larochecleanser">
+            <div class="produkti-info">
+                <p>La Roche Posay Toleriane Hydrating Gentle Facial Cleanser 400ml</p>
+                <p class="zbritja">25.00€</p>  
+                <p class="original-price">31.00€</p> 
+                <p class="short-description">For normal to dry skin.</p>
+                <button class="buy-now">Buy Now</button>
+                <button class="add-to-cart">Add to Cart</button>
+        </div>
+    </div>
+        <div class="produkti"data-brand="fenty">
+            <img src="assets/img/pages/skincare/fentygel.jpg" alt="fg">
+            <div class="produkti-info">
+                <p>Fenty Skin Instant Reset Overnight Recovery Gel-Cream</p>
+                <p>14.00€</p>
+                <p class="short-description">Facial skin care for fine lines , wrinkles , dull and dry skin.</p>
+                <button class="buy-now">Buy Now</button>
+                <button class="add-to-cart">Add to Cart</button>
+        </div>
     </div>
 
-    <div class="contact-form">
-        <h2>Send Us a Message</h2>
-        <form id="contactForm" action="#" method="POST">
-            <label class="contact-label" for="name">Full Name</label>
-            <input class="contact-input" type="text" id="name" name="name" placeholder="Enter your full name" required>
-            <p id="nameError" class="error-message"></p>
-
-            <label class="contact-label" for="email">Email Address</label>
-            <input class="contact-input" type="email" id="email" name="email" placeholder="Enter your email" required>
-            <p id="emailError" class="error-message"></p>
-
-            <label class="contact-label" for="message">Message</label>
-            <textarea class="contact-textarea" id="message" name="message" rows="5" placeholder="Your message..."
-                required></textarea>
-            <p id="messageError" class="error-message"></p>
-            <button class="contact-button" type="submit">Send Message</button>
-        </form>
+        <div class="produkti"data-brand="ordinary">
+            <img src="assets/img/pages/skincare/glycolic.jpg" alt="glycolic">
+            <div class="produkti-info">
+                <p>The Ordinary Glycolic Acid 7% Toning Solution-240ml</p>
+                <p class="zbritja">15.00€</p>  
+                <p class="original-price">22.00€</p> 
+                <p class="short-description">Dark spots whitening , fights acne and exfoliates.</p>
+                <button class="buy-now">Buy Now</button>
+                <button class="add-to-cart">Add to Cart</button>
+        </div>
+    </div>
+   
+        <div class="produkti"data-brand="laroche">
+            <img src="assets/img/pages/skincare/larochespf.jpg" alt="spf">
+            <div class="produkti-info">
+                <p>La Roche-Posay Anthelios Gel-Cream Mattifying Sunscreen Gel 50+</p>
+                <p>31.00€</p>
+                <p class="short-description">For oily sensitive skin.</p>
+                <button class="buy-now">Buy Now</button>
+                <button class="add-to-cart">Add to Cart</button>
+        </div>
+    </div>
+   
+    <div class="produkti" data-brand="cerave">
+        <img src="assets/img/pages/skincare/ceraacne.jpg" alt="ceraa">
+        <div class="produkti-info">
+            <p>CeraVe Face Wash Acne Treatment Salicylic Acid Cleanser with Purifying Clay 237ml</p>
+            <p class="zbritja">13.00€</p>  
+            <p class="original-price">17.00€</p> 
+            <p class="short-description">For oily skin, reduces blackheads and fights acne.</p>
+            <button class="buy-now">Buy Now</button>
+            <button class="add-to-cart">Add to Cart</button>
     </div>
 </div>
 
-<script src="assets/js/contactus.js"></script>
+    <div class="produkti" data-brand="ordinary">
+        <img src="assets/img/pages/skincare/niacinamide.jpg" alt="ni">
+        <div class="produkti-info">
+            <p>The Ordinary Niacinamide 10% + Zinc 1% Oil Control Serum 30ml</p>
+            <p>9.00€</p>
+            <p class="short-description">Controls oil , fades dark spots.</p>
+            <button class="buy-now">Buy Now</button>
+            <button class="add-to-cart">Add to Cart</button>
+    </div>
+</div>
+<div class="produkti" data-brand="fenty">
+    <img src="assets/img/pages/skincare/fentytoner.jpg" alt="toner">
+    <div class="produkti-info">
+        <p>Fenty Skin Fat Water Hydrating Milky Toner Essence with Hyaluronic Acid + Tamarind 150ml</p>
+        <p class="zbritja">20.00€</p>  
+        <p class="original-price">29.00€</p> 
+        <p class="short-description">A toner-essence with a thick, milky texture that’s packed with hydrating ingredients .</p>
+        <button class="buy-now">Buy Now</button>
+        <button class="add-to-cart">Add to Cart</button>
+</div>
+</div>
+<div class="produkti"data-brand="cerave">
+<img src="assets/img/pages/skincare/cera.jpg" alt="cc">
+<div class="produkti-info">
+    <p>Cera Ve Moisturizing Cream With 3 Essential Ceramides 453g</p>
+    <p>15.00€</p>
+    <p class="short-description">Moisturizes & helps restore the protective skin barrier.</p>
+    <button class="buy-now">Buy Now</button>
+    <button class="add-to-cart">Add to Cart</button>
+</div>
+</div>
+<div class="produkti"data-brand="cerave">
+<img src="assets/img/pages/skincare/fentymc.jpg" alt="fmc">
+<div class="produkti-info">
+    <p>Fenty Skin Total Cleans'r Makeup Removing Cleanser 50ml</p>
+    <p>29.00€</p>
+    <p class="short-description">Cleanses and deeply removes make up.</p>
+    <button class="buy-now">Buy Now</button>
+    <button class="add-to-cart">Add to Cart</button>
+</div>
+</div>
+<div class="produkti"data-brand="cerave">
+<img src="assets/img/pages/skincare/ceracleanser.jpg" alt="ccc">
+<div class="produkti-info">
+    <p>Cerave Hydrating Face Wash With Hyaluronic Acid And Glycerin 473ml </p>
+    <p>25.00€</p>
+    <p class="short-description">For normal to dry skin , gently cleanses and hydrates.</p>
+    <button class="buy-now">Buy Now</button>
+    <button class="add-to-cart">Add to Cart</button>
+</div>
+</div>
+<div class="produkti"data-brand="laroche">
+<img src="assets/img/pages/skincare/laa.jpg" alt="laa">
+<div class="produkti-info">
+    <p>La Roche Posay Effaclar Salicylic Acid Acne Treatment Serum 30ml</p>
+    <p>31.00€</p>
+    <p class="short-description">Clears acne blemishes and helps prevent new breakouts, while also visibly reducing pores.</p>
+    <button class="buy-now">Buy Now</button>
+    <button class="add-to-cart">Add to Cart</button>
+</div>
+</div>
+<div class="produkti" data-brand="ordinary">
+<img src="assets/img/pages/skincare/retinol.jpg" alt="retinol">
+<div class="produkti-info">
+    <p>The Ordinary Retinol 0.5% in Squalane 30ml</p>
+    <p>11.00€</p>
+    <p class="short-description">A moderate-strength retinol serum that targets general signs of skin aging.</p>
+    <button class="buy-now">Buy Now</button>
+    <button class="add-to-cart">Add to Cart</button>
+</div>
+</div>
+</div>
+</section>
+<script>
+    document.getElementById("brand-dropdown").addEventListener("change", function () {
+        let selected = this.value;
+        let produktet = document.querySelectorAll(".produkti");
+        //for each loop me i shfaq ose fsheh produktet
+        produktet.forEach(function (product) {
+            if (selected === "all" || product.getAttribute("data-brand") === selected) {
+                product.style.display = "flex"; //shfaqe
+            } else {
+                product.style.display = "none";
+            }
+        });
+    });
+</script>
 <?php
 include_once 'inc/footer.php';
 ?>
